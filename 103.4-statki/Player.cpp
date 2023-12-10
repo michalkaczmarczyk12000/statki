ec77481c53a56468c8954d37981f4f42d80e97de
michal <m.kaczmarczyk12000@gmail.com>
2023-12-10 15:12:40 +0100

    add player class implementation

--- 103.4-statki/Player.cpp
+++ 103.4-statki/Player.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "Player.h"
+
+Player::Player(std::string name) : name_(name), rank_(1000) {};
+Player::Player(std::string name, int rank) : name_(name), rank_(rank) {};
+
+int Player::getRank() const {
+    return rank_;
+}
+
+bool Player::canPlaceShip() {
+    //chwilowo zakładamy, że można stawaić statki wszędzie
+    return true;
+}
+
+void Player::placeShip(Ship ship, std::vector<Field> fields, Map map) {
+    if(canPlaceShip())
+    {
+        //tutaj implmenetujemy coś takiego
+        ships_.push_back(ship);
+
+        //ship.setPosition(fields);
+
+    }
+}
+
+void Player::changeRank(int points) {
+    //zakładamy, że mogą być ujemne punkty
+    rank_ += points;
+}
--- 103.4-statki/Player.h
+++ 103.4-statki/Player.h
@@ -4,10 +4,27 @@
 
 #ifndef INC_103_4_STATKI_PLAYER_H
 #define INC_103_4_STATKI_PLAYER_H
-
+#include <string>
+#include <vector>
+//#include "Ship.h"
+// chwilowy brak implementacji klas zasßąpiony strukturami
+struct Ship{};
+struct Field{};
+struct Map{};
 
 class Player {
-
+public:
+    Player(std::string name);
+    Player(std::string name, int rank);
+    void placeShip(Ship ship, std::vector<Field> fields, Map map);
+    int getRank() const;
+    void changeRank(int points);
+private:
+    std::string name_;
+    std::vector<Ship> ships_;
+    std::vector<Map> maps_;
+    bool canPlaceShip();
+    int rank_;
 };
 
 
