dee46daeb77b4e7960c2e852d15d0ce04f14754d
michal <m.kaczmarczyk12000@gmail.com>
2023-12-10 14:10:22 +0100

    improve file structure

---
+++ 103.4-statki/lib/HumanPlayer.h
@@ -0,0 +1,14 @@
+//
+// Created by michal on 06.12.23.
+//
+
+#ifndef INC_103_4_STATKI_HUMANPLAYER_H
+#define INC_103_4_STATKI_HUMANPLAYER_H
+
+
+class HumanPlayer {
+
+};
+
+
+#endif //INC_103_4_STATKI_HUMANPLAYER_H
---
+++ 103.4-statki/lib/RandomPlayer.h
@@ -0,0 +1,14 @@
+//
+// Created by michal on 06.12.23.
+//
+
+#ifndef INC_103_4_STATKI_RANDOMPLAYER_H
+#define INC_103_4_STATKI_RANDOMPLAYER_H
+
+
+class RandomPlayer {
+
+};
+
+
+#endif //INC_103_4_STATKI_RANDOMPLAYER_H
